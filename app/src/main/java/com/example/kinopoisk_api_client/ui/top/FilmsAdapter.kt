af88607d88e83328f8d10be75c458fe2539a303f
fukenrice <63079551+fukenrice@users.noreply.github.com>
2024-02-10 22:39:51 +0300

    Add films adapter

---
+++ app/src/main/java/com/example/kinopoisk_api_client/ui/top/FilmsAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.kinopoisk_api_client.ui.top
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kinopoisk_api_client.R
+import com.example.kinopoisk_api_client.data.model.Film
+import com.example.kinopoisk_api_client.data.model.FilmListItem
+import com.example.kinopoisk_api_client.databinding.FilmListItemBinding
+import com.squareup.picasso.Picasso
+
+class FilmsAdapter(
+    private val context: Context,
+    private val onItemClicked: (film: FilmListItem) -> Unit,
+    data: List<FilmListItem>
+) : RecyclerView.Adapter<FilmsAdapter.DataViewHolder>() {
+    private val data: MutableList<FilmListItem>
+    init {
+        this.data = data.toMutableList()
+    }
+
+    inner class DataViewHolder(
+        private val onItemClicked: (film: FilmListItem) -> Unit,
+        itemView: View
+    ) :
+        RecyclerView.ViewHolder(itemView) {
+        fun bind(film: FilmListItem) {
+            val binding = FilmListItemBinding.bind(itemView)
+            Picasso.get().load(film.posterUrlPreview).into(binding.ivPoster)
+            binding.tvFilmName.text =
+                if (film.nameRu.length <= 20) film.nameRu else film.nameRu.substring(0, 19) + "..."
+            binding.tvFilmInfo.text =
+                context.getString(R.string.film_desc, film.genre[0].genre, film.year)
+            binding.root.setOnClickListener { onItemClicked(film) }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder =
+        DataViewHolder(
+            onItemClicked,
+            LayoutInflater.from(parent.context).inflate(R.layout.film_list_item, parent, false)
+        )
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    fun clear() = data.clear()
+
+    fun addData(films: List<FilmListItem>) = data.addAll(films)
+}
