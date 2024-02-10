94c25052ef4ebef56732b9fa50859452e4c9db88
fukenrice <63079551+fukenrice@users.noreply.github.com>
2024-02-10 19:50:18 +0300

    Init

---
+++ app/src/main/java/com/example/kinopoisk_api_client/FilmDetailsFragment.kt
@@ -0,0 +1,38 @@
+package com.example.kinopoisk_api_client
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.kinopoisk_api_client.databinding.FragmentFilmDetailsBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class FilmDetailsFragment : Fragment() {
+
+    private var _binding: FragmentFilmDetailsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFilmDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/kinopoisk_api_client/TopFilmsFragment.kt
@@ -0,0 +1,37 @@
+package com.example.kinopoisk_api_client
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.kinopoisk_api_client.databinding.FragmentTopFilmsBinding
+
+class TopFilmsFragment : Fragment() {
+
+    private var _binding: FragmentTopFilmsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentTopFilmsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
